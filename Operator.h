e56ccadf2165a727dc93e4704e432f49012e4a85
glompsgotmail <44075137+glompsgotmail@users.noreply.github.com>
2022-01-28 12:46:21 -0500

    Create Operator.h

---
+++ Operator.h
@@ -0,0 +1,28 @@
+
+#ifndef CHEMCALC_OPERATOR_H
+#define CHEMCALC_OPERATOR_H
+#include <string>
+#include <iostream>
+
+class Operator {
+public:
+    Operator(std::string name, int (*tIntOperation)(int));
+    std::string Name;
+    Operator* InOperator;
+    Operator* OutOperator;
+    std::string InUnit;
+    std::string OutUnit;
+    int input;
+    int output;
+    void performOperation(int input);
+
+
+
+
+
+protected:
+    int (*intOperation)(int);
+};
+
+
+#endif //CHEMCALC_OPERATOR_H
