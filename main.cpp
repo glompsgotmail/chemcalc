38b44d1916a5d5afeee4454c511e56f114c331e1
glompsgotmail <44075137+glompsgotmail@users.noreply.github.com>
2022-01-28 13:12:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Operator.h"
 int main() {
-    Operator o = Operator("dsd", [](int x){return x+1;});
-    o.performOperation();
+    Operator o = Operator("dsd", [](int x []){return x[0]+1;});
+    o.performOperation((int[]){1});
 }
