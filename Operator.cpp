cfa4b747580e85fb6659dbe8eddd0a3923a36002
glompsgotmail <44075137+glompsgotmail@users.noreply.github.com>
2022-01-28 13:12:42 -0500

    Update Operator.cpp

--- Operator.cpp
+++ Operator.cpp
@@ -1,24 +1,22 @@
+//
+// Created by alliang on 1/25/2022.
+//
+
 #include "Operator.h"
 
-Operator::Operator(std::string name, int (*tIntOperation)(int)){
+Operator::Operator(std::string name, int (*tIntOperation)(int [])){
     this->Name = name;
     this->intOperation = tIntOperation;
 };
 
-void Operator::performOperation(int input = NULL) {
+void Operator::performOperation(int input []) {
     int output;
-    if(!input){
-        std::string s;
-       std::cin >> s;
-       try{
-            input = std::stoi(s);
-       }catch(const std::exception& e){
-            std::cout << e.what();
-       }
-    }
+
     output = (*intOperation)(input);
     if(!this->OutOperator) {
         std::cout << output << std::endl;
+    }else{
+        OutOperator->performOperation(output);
     }
 }
 
